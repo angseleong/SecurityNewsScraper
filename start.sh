9c6c338585e2aa3f5bbb6c1942ad486b41487813
angseleong <axeleonjustin148@gmail.com>
2026-08-26 22:09:12 +0700

    fix: bind gunicorn to IPv6 to support alwaysdata reverse proxy

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Run the background scheduler worker in the background
 python -m backend.worker &
 
-# Start the gunicorn web server in the foreground
-exec gunicorn -w 1 -b 0.0.0.0:${PORT:-5000} backend.main:app
+# Start the gunicorn web server in the foreground. Bind to [::] to support AlwaysData IPv6 proxy.
+exec gunicorn -w 1 -b "[::]:${PORT:-5000}" backend.main:app
